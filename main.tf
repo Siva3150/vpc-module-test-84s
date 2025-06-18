54dcd8ea67b5dc95854af760308496e7a6673b1b
Siva prasad <sivaprasad3150@gmail.com>
2025-06-19 00:34:28 +0530

    user-profile

--- main.tf
+++ main.tf
@@ -1,9 +1,16 @@
 module "vpc" {
     source        = "../terraform-aws-vpc-84s"
-    cidr_block = var.cidr_block
-    tags = var.tags 
+
+    /* project_name = "roboshop"
+    environment = "dev"
+    public_subnet_cidrs = ["10.0.1.0/24", "10.0.2.0/24"] */
     project_name = var.project_name
     environment = var.environment
+    public_subnet_cidrs = var.public_subnet_cidrs
+    private_subnet_cidrs = var.private_subnet_cidrs
+    database_subnet_cidrs = var.database_subnet_cidrs
+
+    is_peering_required = true 
 
 
 }
--- variables.tf
+++ variables.tf
@@ -1,17 +1,17 @@
-variable "cidr_block" {
-    type = list(string)
-    default = ["10.0.0.0/16"]
+# variable "cidr_block" {
+#     type = list(string)
+#     #default = ["10.0.0.0/16"]
 
-}
+# }
 
-variable "tags" {
-  default = {
-    Name        = "Roboshop"
-    Terraform   = "true"
-    Environment = "dev"
+# variable "tags" {
+#   default = {
+#     Name        = "Roboshop"
+#     Terraform   = "true"
+#     Environment = "dev"
 
-  }
-}
+#   }
+# }
 
 variable "project_name" {
     default = "Roboshop"
@@ -19,4 +19,22 @@ variable "project_name" {
 
 variable "environment" {
     default = "dev"
+}
+
+variable "public_subnet_cidrs" {
+    type = list(string)
+    default = (["10.0.1.0/24", "10.0.2.0/24"])
+  
+}
+
+variable "private_subnet_cidrs" {
+    type = list(string)
+    default = (["10.0.11.0/24", "10.0.12.0/24"])
+  
+}
+
+variable "database_subnet_cidrs" {
+    type = list(string)
+    default = (["10.0.21.0/24", "10.0.22.0/24"])
+  
 }
